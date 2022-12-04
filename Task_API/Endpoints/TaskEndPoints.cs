9f5650f52effca49eb94141302d6175d52ec31a7
Pedrolustosa <pedroeternalss@gmail.com>
2022-12-04 20:56:14 -0300

    feat: Creating a Minimal API Using Dapper and SQL Server - IV
    * Create new endpoints for tasks

--- Task_API/Endpoints/TaskEndPoints.cs
+++ Task_API/Endpoints/TaskEndPoints.cs
@@ -8,6 +8,7 @@ namespace Task_API.Endpoints
     {
         public static void MapTaskEndPoints(this WebApplication app)
         {
+            
             app.MapGet("/", () => $"Welcome Task API - {DateTime.Now}");
 
             app.MapGet("/tasks", async(GetConnection connectionGetter) =>
@@ -19,6 +20,38 @@ namespace Task_API.Endpoints
                     return Results.NotFound();
 
                 return Results.Ok(tasks);
+            });
+
+            app.MapGet("/task{id}", async (GetConnection connectionGetter, int id) =>
+            {
+                using var con = await connectionGetter();
+                return con.Get<Tasks>(id) is Tasks tasks ? Results.Ok(tasks) : Results.NotFound();
+            });
+
+            app.MapPost("/tasks", async (GetConnection connectionPost, Tasks tasks) =>
+            {
+                using var con = await connectionPost();
+                var id = con.Insert(tasks);
+                return Results.Created($"/tasks/{id}", tasks);
+            });
+
+            app.MapPut("/tasks", async (GetConnection connectionPut, Tasks tasks) =>
+            {
+                using var con = await connectionPut();
+                var id = con.Update(tasks);
+                return Results.NoContent();
+            });
+
+            app.MapDelete("/task/{id}", async (GetConnection connectionDelete, int id) =>
+            {
+                using var con = await connectionDelete();
+                var taskDeleted = con.Get<Tasks>(id);
+
+                if (taskDeleted is null)
+                    Results.NotFound();
+
+                con.Delete(taskDeleted);
+                return Results.Ok($"Task of the id:{ taskDeleted?.Id } was deleted");
             });
         }
     }
